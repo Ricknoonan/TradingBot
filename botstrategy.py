4441ede03311973445f8a65f70c515ff612cc035
Richard Noonan <noonanri@tcd.ie>
2020-10-11 23:02:02 +0100

    Indicator weights

--- botstrategy.py
+++ botstrategy.py
@@ -1,60 +1,81 @@
 from botlog import BotLog
-from botindicators import BotIndicators
+from botindicators import movingAverage, momentum, EMA, MACD, RSI
 from bottrade import BotTrade
 
+MAXTRADESPERPAIR = 1
+
+
 class BotStrategy(object):
-	def __init__(self):
-		self.output = BotLog()
-		self.prices = []
-		self.closes = []  # Needed for Momentum Indicator
-		self.trades = []
-		self.currentPrice = ""
-		self.currentClose = ""
-		self.numSimulTrades = 1
-		self.accumProfit = 0
-		self.closedPosCounter = 0
+    def __init__(self):
+        self.output = BotLog()
+        self.prices = []
+        self.closes = []  # Needed for Momentum Indicator
+        self.trades = []
+        self.currentPrice = ""
+        self.currentClose = ""
+        self.accumProfit = 0
+        self.closedPosCounter = 0
 
-		self.indicators = BotIndicators()
+    def tick(self, candlestick):
+        self.currentPrice = float(candlestick.priceAverage)
+        self.prices.append(self.currentPrice)
 
-	def tick(self, candlestick):
-		self.currentPrice = float(candlestick.priceAverage)
-		self.prices.append(self.currentPrice)
-		
-		#self.currentClose = float(candlestick['close'])
-		#self.closes.append(self.currentClose)
-		#if self.indicators.movingAverage(self.prices, 15) is not None:
-		self.output.log("Price: "+str(candlestick.priceAverage)+"\tMoving Average: "+str(self.indicators.movingAverage(self.prices, 15)) + "\n")
+        # self.currentClose = float(candlestick['close'])
+        # self.closes.append(self.currentClose)
+        # if self.indicators.movingAverage(self.prices, 15) is not None:
+        self.output.log("Price: " + str(candlestick.priceAverage) + "\tMoving Average: " + str(
+            movingAverage(self.prices, 15)) + "\n")
 
-		self.evaluatePositions()
-		self.showPositions()
+        self.evaluatePositions()
+        self.showPositions()
 
-	def evaluatePositions(self):
-		openTrades = []
+    def evaluatePositions(self):
 
-		for trade in self.trades:
-			if trade.status == "OPEN":
-				openTrades.append(trade)
+        # build for checking multiple pairs on each tick
+        openTrades = []
 
-		if len(openTrades) < self.numSimulTrades:
-			if self.indicators.RSI(self.prices) < 30:
-				self.trades.append(BotTrade(self.currentPrice, stopLossPrice=.001))
+        for trade in self.trades:
+            if trade.status == "OPEN":
+                openTrades.append(trade)
 
+        if len(openTrades) < MAXTRADESPERPAIR:
+            weight = 0
+            weight += self.checkRSI(self.prices)
+            weight += self.checkMACD(self.prices)
+            weight += self.checkMomentum(self.prices)
+            if weight >= 70:
+                self.trades.append(BotTrade(self.currentPrice, stopLossPrice=.001))
 
-		# TODO: Refactor Clean up close/stoploss
-		for trade in openTrades:
-			if self.currentPrice >= (trade.entryPrice * 1.01):
-				trade.close(self.currentPrice)
-				self.accumProfit += trade.profit
-				self.closedPosCounter += 1
-				self.output.log("Strategy Profit/Loss: " + str(self.accumProfit) + "\n" + str(self.closedPosCounter) + "\n")
+        # TODO: Refactor Clean up close/stoploss
+        for trade in openTrades:
+            if self.currentPrice >= (trade.entryPrice * 1.01):
+                trade.close(self.currentPrice)
+                self.accumProfit += trade.profit
+                self.closedPosCounter += 1
+                self.output.log(
+                    "Strategy Profit/Loss: " + str(self.accumProfit) + "\n" + str(self.closedPosCounter) + "\n")
 
-			#if float(self.currentPrice) > float(self.indicators.movingAverage(self.prices, 15)):
-			#	trade.close(self.currentPrice)
-			if trade.status == "OPEN":
-				trade.stopLoss(self.currentPrice)
+            # if float(self.currentPrice) > float(self.indicators.movingAverage(self.prices, 15)):
+            #	trade.close(self.currentPrice)
+            if trade.status == "OPEN":
+                trade.stopLoss(self.currentPrice)
 
+    def showPositions(self):
+        for trade in self.trades:
+            trade.showTrade()
 
+    def checkRSI(self, prices):
+        pass
 
-	def showPositions(self):
-		for trade in self.trades:
-			trade.showTrade()
+    def checkMACD(self, prices):
+        pass
+
+    def checkMomentum(self, prices):
+        pass
+
+#If underlying prices make a new high or low that isn't
+# confirmed by the RSI, this divergence can signal a price reversal.
+# If the RSI makes a lower high and then follows with a downside move below
+# a previous low, a Top Swing Failure has occurred. If the RSI makes a higher
+# low and then follows with an upside move above a previous high, a Bottom
+# Swing Failure has occurred.
--- bottrade.py
+++ bottrade.py
@@ -1,5 +1,6 @@
 from botlog import BotLog
 
+
 class BotTrade(object):
     def __init__(self, currentPrice, stopLossPrice=0):
         self.output = BotLog()
@@ -13,22 +14,23 @@ class BotTrade(object):
             self.stopLossPrice = currentPrice - stopLossPrice
             # TODO: add in percentage stop losses
 
-
     # TODO: Refactor this output/ combine close and stopLoss
     def close(self, currentPrice):
         self.status = "CLOSED"
         self.exitPrice = currentPrice
         self.profit = self.exitPrice - self.entryPrice
         self.output.log("Trade closed!!!")
-        self.output.log("Entry Price: " + str(self.entryPrice) + " Status: " + self.status + " Exit Price: " + str(self.exitPrice) + " P/L: " + str(self.profit) + "\n")
+        self.output.log("Entry Price: " + str(self.entryPrice) + " Status: " + self.status + " Exit Price: " + str(
+            self.exitPrice) + " P/L: " + str(self.profit) + "\n")
 
     def stopLoss(self, currentPrice):
         if self.stopLossPrice:
             if currentPrice < self.stopLossPrice:
                 self.close(currentPrice)
-                self.output.log("Stop loss hit! Status: " + self.status + " Exit Price: " + str(self.exitPrice) + "Loss: " + str(self.profit))
+                self.output.log(
+                    "Stop loss hit! Status: " + self.status + " Exit Price: " + str(self.exitPrice) + "Loss: " + str(
+                        self.profit))
 
     def showTrade(self):
-        if(self.status =="OPEN"):
+        if self.status == "OPEN":
             self.output.log(self.tradeStatus)
-
